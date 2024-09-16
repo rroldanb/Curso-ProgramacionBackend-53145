79baeec6fc370ba6ac3333b0ab7b07a8ff976665
Ruben Roldan <ruben.roldan.b@gmail.com>
2024-09-15 21:45:08 -0300

    Rubén Roldán - Proyecto Final - detalles

--- src/public/js/util.js
+++ src/public/js/util.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const baseUrl = window.location.origin;
   
-    const imagePath = "/img/warriorRR.png";
+    const imagePath = "img/warriorRR.png";
   console.log("direccion imagen", baseUrl + imagePath)
     document.querySelectorAll('.hero-image').forEach(function(img) {
       img.src = baseUrl + imagePath;
